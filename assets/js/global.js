87f9ad82c57c33a126cace0824c539b44d61b93f
Ridwan <aladebaruwa@gmail.com>
2024-07-12 18:09:57 +0100

    Updated CSS, JS, and HTML files: removed CSS comment, added event listener function in JS, and modified HTML elements' classes and attributes.

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,3 +5,18 @@
 /*
     * Add event listener on multiple elements
 */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (const elem of elements)  elem.addEventListener(eventType, callback);
+}
+
+/**
+ *  Toggle search box in mobile device || small screen
+ */
+
+const searchBox = document.querySelector("search-box");
+const searchTogglers = document.querySelector("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", function () {
+    searchBox.classList.toggle("active");
+})
