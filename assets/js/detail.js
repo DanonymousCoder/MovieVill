68403872c65cef9d99f1fd2f0afcb92ec33cd334
Ridwan <aladebaruwa@gmail.com>
2024-07-16 16:18:53 +0100

    Updated fetchDataFromServer API call and extracted movie data into variables

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -14,6 +14,22 @@ sidebar();
 
 
 
-fetchDataFromServer(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${acd7051084d12e733b81592b8c9d8b60}&append_to_response=casts,videos,images,releases`, function(movie) {
+fetchDataFromServer(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}&append_to_response=casts,videos,images,releases`, function(movie) {
+
+    const {
+        backdrop_path,
+        poster_path,
+        title,
+        release_date,
+        runtime,
+        vote_average,
+        releases: { countries: [{ certification }]},
+        genres,
+        overview,
+        casts: { cast, crew },
+        videos: { results: videos }
+    } = movie
+
+    document.title = `${title} - Tvflix`
 
 })
